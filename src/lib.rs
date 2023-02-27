aebce450458f0991e313010d11e11edab530175e
Mondobe <68780462+mondobe@users.noreply.github.com>
2023-02-27 10:33:50 -0500

    Added more documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 pub mod token;
 pub use token::*;
 
+/// Processes a rule across a vector of tokens. Starting from the first token,
+/// iteratively applies the rule on a single token. If that application returns
+/// None, continues applying the rule on the token and the next, then the next,
+/// and so on until the function returns Some or there are no tokens left to
+/// process. This process is explained more thoroughly with examples in the 
+/// readme.
 pub fn process_rule(rule: impl Fn(Vec<Token>) -> Option<Vec<Token>>, body: &mut Vec<Token>) {
     // iterate through each starting position in the body
     let mut start_index: usize = 0;
@@ -32,6 +38,7 @@ pub fn process_rule(rule: impl Fn(Vec<Token>) -> Option<Vec<Token>>, body: &mut 
     }
 }
 
+/// Processes multiple rules on a vector of tokens. See [process_rule].
 pub fn process_rules(rules: Vec<impl Fn(Vec<Token>) -> Option<Vec<Token>>>, body: &mut Vec<Token>) {
     for rule in rules {
         process_rule(rule, body);
@@ -45,17 +52,11 @@ mod tests {
     use super::*;
 
     fn token1() -> Token<'static> {
-        token_from_string(
-            "Hi!",
-            vec!["greeting", "exclam"]
-        )
+        token_from_string("Hi!", vec!["greeting", "exclam"])
     }
 
     fn token2() -> Token<'static> {
-        token_from_string(
-            "-890",
-            vec!["int", "neg_int"]
-        )
+        token_from_string("-890", vec!["int", "neg_int"])
     }
 
     fn test_tokens() -> Vec<Token<'static>> {
@@ -74,9 +75,7 @@ mod tests {
 
     #[test]
     fn print_base_token_stream() {
-        print_tokens(
-            str_to_tokens("This is a string.")
-        );
+        print_tokens(str_to_tokens("This is a string."));
     }
 
     fn whitespace_rule(mut tokens: Vec<Token>) -> Option<Vec<Token>> {
@@ -102,7 +101,7 @@ mod tests {
         } else {
             Some(vec![
                 wrap(tokens[0..tokens.len() - 1].to_vec(), vec!["word"]),
-                tokens.last().unwrap().clone()
+                tokens.last().unwrap().clone(),
             ])
         }
     }
@@ -117,16 +116,19 @@ mod tests {
                 } else {
                     Some(tokens)
                 }
-            },
+            }
             TokenStructure::Multiple => {
                 if tokens.last().unwrap_or(&empty_token()).has_tag("digit") {
                     None
                 } else {
                     Some(vec![
                         wrap(tokens[0..tokens.len() - 1].to_vec(), vec!["int", "posInt"]),
-                        tokens.last().unwrap().clone()
+                        tokens.last().unwrap().clone(),
                     ])
                 }
+            }
+            TokenStructure::None => {
+                Some(tokens)
             }
         }
     }
@@ -165,8 +167,9 @@ mod tests {
             whitespace_rule,
             letter_rule,
             word_rule,
-            remove_whitespace_rule
-        ].to_vec()
+            remove_whitespace_rule,
+        ]
+        .to_vec()
     }
 
     fn int_rules() -> Vec<impl Fn(Vec<Token>) -> Option<Vec<Token>>> {
@@ -174,10 +177,11 @@ mod tests {
             whitespace_rule,
             digit_rule,
             int_rule,
-            remove_whitespace_rule
-        ].to_vec()
+            remove_whitespace_rule,
+        ]
+        .to_vec()
     }
-    
+
     fn ab_rule(tokens: Vec<Token>) -> Option<Vec<Token>> {
         match tokens_structure(&tokens) {
             TokenStructure::Single(tok) => {
@@ -186,13 +190,16 @@ mod tests {
                 } else {
                     Some(tokens)
                 }
-            },
+            }
             TokenStructure::Multiple => {
                 if tokens[1].has_tag("b") {
                     Some(vec![wrap(tokens, vec!["c"])])
                 } else {
                     Some(tokens)
                 }
+            }
+            TokenStructure::None => {
+                Some(tokens)
             }
         }
     }
@@ -229,8 +236,7 @@ mod tests {
 
     #[test]
     fn big_paragraph_performance() {
-        let text = 
-        "The donkey than rams him.
+        let text = "The donkey than rams him.
         Oh my goodness, but the kangaroo jumps over.
         And it looks like the seagulls are going for it again!
         They're just hitting the tank!
@@ -242,7 +248,7 @@ mod tests {
         Get over here, T-Rex. I'll beat you up.
         Now watch out for my spin attack...";
         let mut body = str_to_tokens(text);
-        for _ in 0..1000 {        
+        for _ in 0..1000 {
             black_box(process_rules(word_rules(), &mut body));
         }
     }
@@ -284,5 +290,4 @@ mod tests {
         println!("{}", token_from_string("Hi", vec!["test"]).has_tag("test"));
         print_tokens(str_to_tokens("a b blex ab abab"));
     }
-
 }
--- src/token.rs
+++ src/token.rs
@@ -1,98 +1,127 @@
-use std::ops::Range;
 use std::fmt;
+use std::ops::Range;
 
 #[derive(Clone, PartialEq, Debug)]
+
+/// A representation of a token. The `body` field points to the underlying string
+/// from which the characters in this token come. The `indices` field contains
+/// a range representing which contiguous characters are used. The `tags` field is
+/// a vector of borrowed strings with user-defined values.
 pub struct Token<'a> {
     pub body: &'a str,
     pub indices: Range<usize>,
-    pub tags: Vec<&'a str>
+    pub tags: Vec<&'a str>,
 }
 
 impl Default for Token<'_> {
+    /// A token's default value is the empty token, which borrows 0 characters
+    /// from a new empty string.
     fn default() -> Self {
         empty_token()
     }
 }
 
-impl <'a>Token<'a> {
+impl<'a> Token<'a> {
+    /// Gets the content of the token as a string slice from the body. Mostly used
+    /// for printing tokens.
     pub fn content(&'a self) -> &'a str {
         &self.body[self.indices.clone()]
     }
 
+    /// Whether or not a token's tags contain a certain string slice value.
     pub fn has_tag(&self, tag: &str) -> bool {
         self.tags.contains(&tag)
     }
 
+    /// If this token is a single character long, returns that character. Otherwise,
+    /// returns None.
     pub fn single_char(&self) -> Option<char> {
         self.content().chars().next()
     }
 }
 
+/// A token with no contents and a static body.
 pub fn empty_token() -> Token<'static> {
     Token {
         body: "",
         indices: 0..0,
-        tags: vec![]
+        tags: vec![],
     }
 }
 
-impl <'a>fmt::Display for Token<'a> {
+impl<'a> fmt::Display for Token<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}: ", self.content()).unwrap();
         for tag in &self.tags {
             write!(f, "{}; ", tag).unwrap();
-        };
+        }
         Ok(())
     }
 }
 
+/// Consolidates a single string slice and a vector of tags into one token.
+/// Mostly used for debugging.
 pub fn token_from_string<'a>(content: &'a str, tags: Vec<&'a str>) -> Token<'a> {
     Token {
         body: content,
         indices: 0..content.len(),
-        tags
+        tags,
     }
 }
 
+/// Prints a vector of tokens using their default Display method.
 pub fn print_tokens(tokens: Vec<Token>) {
     for tok in tokens {
         println!("{}", tok);
     }
 }
 
+/// Chops up a string slice into a vector of owned tokens. Also appends an empty
+/// token to the tail of the vector to enable certain lexing functions like
+/// scanning for words.
 pub fn str_to_tokens<'a>(body: &'a str) -> Vec<Token> {
     let mut tokens: Vec<Token> = vec![];
     for index in 0..body.len() {
         tokens.push(Token {
             body,
             indices: index..index + 1,
-            tags: vec![&body[index..index + 1]]
+            tags: vec![&body[index..index + 1]],
         });
     }
     tokens.push(empty_token());
     tokens
 }
 
+/// Wraps up a vector of consecutive tokens into one token and applies the
+/// specified tags.
 pub fn wrap<'a>(tokens: Vec<Token<'a>>, tags: Vec<&'a str>) -> Token<'a> {
     if tokens.is_empty() {
         return empty_token();
     }
-    Token { 
-        body: tokens[0].body, 
-        indices: tokens[0].indices.start..tokens.last().unwrap().indices.end, 
-        tags
+    Token {
+        body: tokens[0].body,
+        indices: tokens[0].indices.start..tokens.last().unwrap().indices.end,
+        tags,
     }
 }
 
+/// Represents the possible shapes of a vector of tokens. A vector of tokens could
+/// contain multiple tokens, a single token (a common special case), or none at
+/// all (a rare corner case).
 pub enum TokenStructure<'a> {
     Multiple,
-    Single(&'a Token<'a>)
+    Single(&'a Token<'a>),
+    None,
 }
 
+/// Goes with the TokenStructure enum. Specifies whether a borrowed vector of
+/// tokens consists of multiple tokens, a single token, or none at all.
 pub fn tokens_structure<'a>(tokens: &'a Vec<Token<'a>>) -> TokenStructure<'a> {
     if tokens.len() == 1 {
         TokenStructure::Single(&tokens[0])
-    } else {
+    } else if tokens.len() > 1 {
         TokenStructure::Multiple
+    } else {
+        TokenStructure::None
     }
 }
